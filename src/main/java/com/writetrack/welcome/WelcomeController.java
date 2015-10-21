91c9a0931c40d7a6127005c760f8d23575e73b8a
jabbaugh <jbaugh@sovrn.com>
2015-10-21 08:45:14 -0600

    Getting Started with SB

---
+++ src/main/java/com/writetrack/welcome/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.writetrack.welcome;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome";
+    }
+
+}
