06783786bcfc922257c53b73a4fe82fa741211e1
jabbaugh <jbaugh@sovrn.com>
2015-10-27 20:55:29 -0600

    WIP 2

--- src/main/java/com/writetrack/Application.java
+++ src/main/java/com/writetrack/Application.java
@@ -1,8 +1,15 @@
 package com.writetrack;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 import java.util.Arrays;
 
@@ -17,6 +24,28 @@ public class Application {
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
+        }
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("writetrack")
+                    .secret("wtsecret")
+                    .authorizedGrantTypes("authorization_code", "refresh_token",
+                            "password").scopes("openid");
         }
     }
 }
